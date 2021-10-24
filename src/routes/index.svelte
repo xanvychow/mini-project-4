432e9b508035ed355d59061dead138f6849af5ec
xanvy <xanvychow@gmail.com>
2021-10-24 09:52:50 +0000

    vercel deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,2 @@
-<script>
-    import Color from '$lib/Color.svelte';
-    let colorName = 'Pink';
-    let colorName2 = 'Blue';
-</script>
-
-<div>
-    <h1 class="text-primary">Play with Colors</h1>
-</div>
-
-<div>
-    <input bind:value={colorName} />
-    <input bind:value={colorName2} />
-    <Color color={colorName} />
-    <Color color={colorName2}/>
-</div>
-
-<style>
-    input {
-   	 padding: 0.5em;
-   	 text-align: center;
-    }
-    div {
-   	 text-align: center;
-   	 margin: 3em auto;
-    }
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
